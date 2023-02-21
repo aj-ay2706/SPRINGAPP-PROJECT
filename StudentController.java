f22437689f372c112cc03abbf726aaf538dfdb8c
aj-ay2706 <118709615+aj-ay2706@users.noreply.github.com>
2023-02-21 10:56:14 +0530

    Add files via upload

--- StudentController.java
+++ StudentController.java
@@ -1,29 +1,15 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class StudentController {
-	@Value("${value.name}")
-	private String newnameString;
-	@GetMapping("/")
-	public String homepage() {
-	    return newnameString+" to student list";
+	@GetMapping("/get")
+	public StudentResponse getStudent()
+	{
+		StudentResponse studentResponse=new StudentResponse(1,"John","Smith");
+		return studentResponse;
+	}
 
-}
-      @GetMapping("/List")
-      public List<student> getList(){
-    	  List<student> detailsList=new ArrayList<>();
-    	  detailsList.add(new student(1,"XXXX",23));
-    	  detailsList.add(new student(2,"YYYY",3));
-    	  detailsList.add(new student(3,"ZZZZ",25));
-    	  
-    	  return detailsList;
-      }
-	
 }
